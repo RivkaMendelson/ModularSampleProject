1c85c9025f56541746569009caf8d918cb7e9ca8
Rivka Mendelson <rivkamendelson123@gmail.com>
2023-12-14 23:37:28 -0500

    Add project files.

---
+++ ModularSampleProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModularSampleProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ModularSampleProject/Models/Index.cs
@@ -0,0 +1,6 @@
+﻿namespace ModularSampleProject.Models
+{
+    public class Index
+    {
+    }
+}
